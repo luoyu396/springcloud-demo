cc3b021357107a7e10afe46e736a1f7a9a0030ea
tanbb <980644731@qq.com>
2020-05-19 22:52:40 +0800

    sleep

--- springcloud-provider/src/main/java/com/example/springcloud/provider/controller/ProviderController.java
+++ springcloud-provider/src/main/java/com/example/springcloud/provider/controller/ProviderController.java
@@ -4,14 +4,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Random;
 
 @RestController
 public class ProviderController {
 
     @GetMapping("/getInfo")
-    public String getInfo(HttpServletRequest request) {
+    public String getInfo(HttpServletRequest request) throws InterruptedException {
         String res = "providerInfo";
         res += request.getLocalPort();
+        int sleepTime = new Random().nextInt(3000);
+        Thread.sleep(sleepTime);
         System.out.println(System.currentTimeMillis());
         return res;
     }
