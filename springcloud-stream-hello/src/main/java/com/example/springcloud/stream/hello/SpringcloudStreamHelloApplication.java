b9e43b000f04747e6e48202bf150693fd8ec4225
tanbb <980644731@qq.com>
2020-06-19 21:51:23 +0800

    stream

---
+++ springcloud-stream-hello/src/main/java/com/example/springcloud/stream/hello/SpringcloudStreamHelloApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloud.stream.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudStreamHelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudStreamHelloApplication.class, args);
+    }
+
+}
