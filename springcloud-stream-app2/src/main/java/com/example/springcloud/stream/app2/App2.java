6f2e56e0671a9953f6027161e1df4674c66452a8
tanbb <980644731@qq.com>
2020-06-21 22:00:42 +0800

    stream

---
+++ springcloud-stream-app2/src/main/java/com/example/springcloud/stream/app2/App2.java
@@ -0,0 +1,18 @@
+package com.example.springcloud.stream.app2;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Processor;
+
+@EnableBinding(value = {Processor.class})
+public class App2 {
+
+    private static Logger logger = LoggerFactory.getLogger(App2.class);
+
+    @StreamListener(Processor.INPUT)
+    public void receiver(Object info) {
+        logger.info("receiver:" + info);
+    }
+}
